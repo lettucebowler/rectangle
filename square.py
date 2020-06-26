b8e0a819302d47d982e3d3a2264a77d9730cf000
Grant Montgomery <montg433@d.umn.edu>
2020-06-25 22:17:51 -0500

    I think it works now

--- square.py
+++ square.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 from math import sqrt
+
 # input 4 tuples representing the ordered pairs of the 4 points, unordered.
 def checkForRectangle(point_list):
+    is_rectangle = False
+    if len(point_list)  != 4:
+        return false
+
     a = point_list[0]
     distance_list = []
     for x, y in point_list[1:]:
         distance_list.append(sqrt((x - a[0]) ** 2 + (y - a[1]) ** 2))
         print("{}".format(distance_list[-1]))
-    # TODO: check if any distance in distance_list squared is equal to the sum of other distances squared 
 
-
+    distance_list = sorted(distance_list, reverse=True)
+    return distance_list[0] == sqrt((distance_list[1] ** 2 + distance_list[2] ** 2))
 
 # Driver Code
-point_list = [(0, 0), (3, 0), (3, 3), (0, 3)]
+# point_list = [(0, 0), (4, 3), (1, 7), (-3, 4)]
+point_list = [(0, 0), (4, 3), (-6, 8), (-2, 11)]
+
 if checkForRectangle(point_list):
-    print("is rectangle")
+    print("is rectangle" + str(2 ** 2))
